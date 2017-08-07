40ff80509f39c9b5ad0d5103b1d9cf0286d6baa2
Charmie <charmiequino@gmail.com>
2017-08-07 23:26:26 +0800

    Added js for active selection on nav bar

---
+++ main.js
@@ -0,0 +1,18 @@
+
+function setActive(val) {
+  if(val.id == 'id_portfolio') {
+    document.getElementById('id_portfolio').classList.toggle("w3-text-teal");
+    document.getElementById('id_about').classList.remove("w3-text-teal");
+    document.getElementById('id_contact').classList.remove("w3-text-teal");
+  } else if(val.id == 'id_about') {
+    document.getElementById('id_portfolio').classList.remove("w3-text-teal");
+    document.getElementById('id_about').classList.toggle("w3-text-teal");
+    document.getElementById('id_contact').classList.remove("w3-text-teal");
+  } else if(val.id == 'id_contact') {
+    document.getElementById('id_portfolio').classList.remove("w3-text-teal");
+    document.getElementById('id_about').classList.remove("w3-text-teal");
+    document.getElementById('id_contact').classList.toggle("w3-text-teal");
+  }
+
+  console.log(y);
+}
