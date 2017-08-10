88e81f18d33d416f0195f084613c8999e303db92
Charmie <charmiequino@gmail.com>
2017-08-11 04:26:53 +0800

    added thumbnails in portfolio updated about description updated script to handle navBtns

--- main.js
+++ main.js
@@ -1,17 +1,40 @@
 
 function setActive(val) {
-  if(val.id == 'id_portfolio') {
-    document.getElementById('id_portfolio').classList.toggle("w3-text-teal");
-    document.getElementById('id_about').classList.remove("w3-text-teal");
-    document.getElementById('id_contact').classList.remove("w3-text-teal");
-  } else if(val.id == 'id_about') {
-    document.getElementById('id_portfolio').classList.remove("w3-text-teal");
-    document.getElementById('id_about').classList.toggle("w3-text-teal");
-    document.getElementById('id_contact').classList.remove("w3-text-teal");
-  } else if(val.id == 'id_contact') {
-    document.getElementById('id_portfolio').classList.remove("w3-text-teal");
-    document.getElementById('id_about').classList.remove("w3-text-teal");
-    document.getElementById('id_contact').classList.toggle("w3-text-teal");
+  var navBtns = ['id_portfolio', 'id_about', 'id_contact'];
+  for(var i = 0; i < navBtns.length; i++) {
+    if(val.id == navBtns[i]) {
+        document.getElementById(navBtns[i]).classList.add("w3-text-teal");
+    } else {
+        document.getElementById(navBtns[i]).classList.remove("w3-text-teal");
+    }
+  }
+}
+
+function showPortfolioSection(val) {
+  console.log(val);
+  var arrayBtns = ["all-btn", "web-btn", "health-it-btn", "support-btn"];
+  var arraySections = ["", "web-section", "health-it-section", "support-section"];
+  var idStr = val.id;
+  for(var i = 0; i < arrayBtns.length; i++) {
+    if(val.id == arrayBtns[i]) {
+      document.getElementById(arrayBtns[i]).classList.add('w3-black');
+      document.getElementById(arrayBtns[i]).classList.remove('w3-white');
+    } else {
+      document.getElementById(arrayBtns[i]).classList.remove('w3-black');
+      document.getElementById(arrayBtns[i]).classList.add('w3-white');
+    }
+
+    if(val.id == arrayBtns[0]) {
+      if(i < 3) {
+        document.getElementById(arraySections[i+1]).classList.remove('hide-block');
+      }
+    } else {
+      if(val.id == arrayBtns[i] && i != 0) {
+        document.getElementById(arraySections[i]).classList.remove('hide-block');
+      } else if(val.id != arrayBtns[i] && i != 0) {
+        document.getElementById(arraySections[i]).classList.add('hide-block');
+      }
+    }
   }
 
 }
